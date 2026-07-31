f11cafc2a9c2587f05e1b6f4026a517c97e284e7
0xCanet <0xcanet@gmail.com>
2026-07-31 23:54:55 +0200

    Disable metered image optimization

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const nextConfig: NextConfig = {
   
   // Optimisation des images
   images: {
+    unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     qualities: [75, 85, 90],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
